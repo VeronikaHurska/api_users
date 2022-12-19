d78a7beb7faa7ac8897416a628bf07a89156b4d9
VeronikaHurska <nika.gurska@gmail.com>
2022-12-19 14:41:44 +0200

    proj

--- details.js
+++ details.js
@@ -4,6 +4,7 @@ let check = (value) => {
     }
     return (typeof value === 'object')
 }
+let divFull = document.querySelector('.full-user-info')
 let print = (obj) => {
     for (const value in obj) {
         if (check(obj[value])) {
@@ -11,27 +12,35 @@ let print = (obj) => {
         } else {
             let div = document.createElement('div');
             div.innerText = `${value} -- ${obj[value]}`
-            document.body.append(div);
+            divFull.append(div);
 
         }
     }
 }
-let btn_posts = document.querySelector('#postsTitle')
+
 let url = new URL(location.href);
 let id = url.searchParams.get('id')
+let btn_posts = document.createElement('button');
+btn_posts.innerText = ' POSTS';
+btn_posts.classList.add('post-button')
 fetch(`https://jsonplaceholder.typicode.com/users/` + id)
     .then(response => response.json())
     .then(user => {
 
         print(user)
+        let postsDiv = document.createElement('div');
+        postsDiv.classList.add()
+        document.body.appendChild(postsDiv)
+        postsDiv.appendChild(btn_posts);
+
         btn_posts.onclick = () => {
             fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
                 .then(response => response.json())
                 .then(posts => {
-                    console.log(posts)
                     for (const post of posts) {
                         let posts_div = document.createElement('div')
                         posts_div.innerHTML = post.title;
+                        posts_div.classList.add("posts-short-info")
                         let btn_post_details = document.createElement('button')
                         btn_post_details.innerHTML = 'post details'
 
--- post-details.js
+++ post-details.js
@@ -1,13 +1,21 @@
 let url = new URL(location.href);
 let id = url.searchParams.get('id')
-fetch(`https://jsonplaceholder.typicode.com/posts` + id)
+fetch(`https://jsonplaceholder.typicode.com/posts/` + id)
     .then((response) => response.json())
     .then(post => {
         console.log(post)
-        for (const key of post) {
-            console.log(key);
-            for (const keyKey in key) {
 
-            }
+        for (const key in post) {
+            let divPost = document.createElement('div')
+            divPost.innerText =`${key} : ${post[key]}`
+            document.body.appendChild(divPost)
         }
+        fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+            .then(response => response.json())
+            .then(comments => {
+                console.log(comments);
+            })
+
+
+
     })

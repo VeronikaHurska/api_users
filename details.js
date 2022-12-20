006950e981a2e3685869e62442a5e073eb8ec526
VeronikaHurska <nika.gurska@gmail.com>
2022-12-20 12:57:27 +0200

    proj

--- details.js
+++ details.js
@@ -12,6 +12,7 @@ let print = (obj) => {
         } else {
             let div = document.createElement('div');
             div.innerText = `${value} -- ${obj[value]}`
+            div.classList.add('user-element')
             divFull.append(div);
 
         }
@@ -21,31 +22,36 @@ let print = (obj) => {
 let url = new URL(location.href);
 let id = url.searchParams.get('id')
 let btn_posts = document.createElement('button');
-btn_posts.innerText = ' POSTS';
+btn_posts.innerText = ' POSTS OF CURRENT USER';
 btn_posts.classList.add('post-button')
 fetch(`https://jsonplaceholder.typicode.com/users/` + id)
     .then(response => response.json())
     .then(user => {
 
         print(user)
-        let postsDiv = document.createElement('div');
-        postsDiv.classList.add()
-        document.body.appendChild(postsDiv)
-        postsDiv.appendChild(btn_posts);
+        let divButtonPosts = document.createElement('div');
+        divButtonPosts.classList.add('div-for-button')
+        document.body.appendChild(divButtonPosts)
+        divButtonPosts.appendChild(btn_posts);
 
+        let allPostsDiv = document.createElement('div');
+        allPostsDiv.classList.add('all-posts-div');
+        divButtonPosts.appendChild(allPostsDiv);
         btn_posts.onclick = () => {
             fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
                 .then(response => response.json())
                 .then(posts => {
                     for (const post of posts) {
-                        let posts_div = document.createElement('div')
-                        posts_div.innerHTML = post.title;
-                        posts_div.classList.add("posts-short-info")
+
+                        let post_div = document.createElement('div')
+                        post_div.innerHTML = post.title;
+                        post_div.classList.add("posts-short-info")
                         let btn_post_details = document.createElement('button')
                         btn_post_details.innerHTML = 'post details'
 
-                        posts_div.append(btn_post_details);
-                        document.body.append(posts_div)
+                        post_div.append(btn_post_details);
+                        allPostsDiv.appendChild(post_div)
+                         // document.body.appendChild(post_div)
                         btn_post_details.onclick = () => {
                             document.location = `post-details.html?id=${post.id}`
                         }
--- post-details.js
+++ post-details.js
@@ -3,17 +3,32 @@ let id = url.searchParams.get('id')
 fetch(`https://jsonplaceholder.typicode.com/posts/` + id)
     .then((response) => response.json())
     .then(post => {
-        console.log(post)
-
+        let postDiv = document.createElement('div')
+        postDiv.classList.add('post-block')
         for (const key in post) {
             let divPost = document.createElement('div')
+            divPost.classList.add("div-comment-key")
+
             divPost.innerText =`${key} : ${post[key]}`
-            document.body.appendChild(divPost)
+            postDiv.appendChild(divPost)
+            document.body.appendChild(postDiv)
         }
         fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
             .then(response => response.json())
             .then(comments => {
-                console.log(comments);
+                for (const comment of comments) {
+                    let divOneComment = document.createElement('div')
+                    divOneComment.classList.add('div-one-comment')
+                    for (const commentKey in comment) {
+                        let div = document.createElement('div')
+                        div.classList.add("div-comment-key")
+                        div.innerText = `${commentKey}: ${comment[commentKey]}`
+                        divOneComment.appendChild(div)
+                        document.body.appendChild(divOneComment)
+
+                    }
+
+                }
             })
 
 
